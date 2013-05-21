75ae1259257d5d2c4c84c4e365d103227e5eba6c
dvsdkval <dvsdkval@dvsdkval>
2013-05-21 19:10:23 +0530

    script_remote.sh added

--- script_remote.sh
+++ script_remote.sh
@@ -1,11 +1,16 @@
 echo "start cloning";
 cd ;
+if(ls | grep temp_clone) 
+	then
+	rm -rf temp_clone;
+	echo "removing temp_clone";
+fi
 mkdir temp_clone;
 cd temp_clone;
-git clone git://gitorious.tif.ti.com/glsdk/gst-ducati.git;
-cd gst-ducati;
-./autogen.sh --prefix=/usr;
-echo "start make and make_install";
-make;
-make install;
-echo "finished";
+#git clone git://gitorious.tif.ti.com/glsdk/gst-ducati.git;
+#cd gst-ducati;
+#./autogen.sh --prefix=/usr;
+#echo "start make and make_install";
+#make;
+#make install;
+#echo "finished";
