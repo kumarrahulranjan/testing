0714f0204563435aa9da52b0064437ebafded2e0
dvsdkval <dvsdkval@dvsdkval>
2013-05-21 16:14:15 +0530

    arbit changes

--- script.sh
+++ script.sh
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
+
 echo "Hi $1 this is my first project";
-=======
-echo "My first script in GIT";
-read n;
-echo $n ;
->>>>>>> 0583578c21024f045e582ec65a3879bebe73973d
+#echo "My first script in GIT";
+#read n;
+#echo $n ;
