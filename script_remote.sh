d08b371ceb76460dbd45929e9d1ec6f1d6377e02
kumarrahulranjan <krriitb@gmail.com>
2013-05-24 17:57:45 +0530

    Update script_remote.sh

--- script_remote.sh
+++ script_remote.sh
@@ -5,18 +5,18 @@ function error_exit
 }
 
 echo "start cloning";
-cd ;
-if(ls | grep temp_clone) 
-	then
-	rm -rf temp_clone;
-	echo "removing temp_clone";
-fi
-mkdir temp_clone || error_exit "mkdir failed";
-cd temp_clone || error_exit "cd to temp_clone failed";
-git clone $1 || error_exit "git clone failed";
-cd gst-ducati || error_exit "cd failed `pwd`";
-echo "applying patch";
-git apply ~/rahul.patch || error_exit "failed to apply patch";
+#cd ;
+#if(ls | grep temp_clone) 
+#	then
+#	rm -rf temp_clone;
+#	echo "removing temp_clone";
+#fi
+#mkdir temp_clone || error_exit "mkdir failed";
+#cd temp_clone || error_exit "cd to temp_clone failed";
+#git clone $1 || error_exit "git clone failed";
+#cd gst-ducati || error_exit "cd failed `pwd`";
+#echo "applying patch";
+#git apply ~/rahul.patch || error_exit "failed to apply patch";
 #echo "running autogen";
 ./autogen.sh --prefix=/usr || error_exit "autogen.sh failed";
 make || error_exit "make failed"; 
